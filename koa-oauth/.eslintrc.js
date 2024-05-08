23bac977822c662c803003fda733aca157a90278
Matt Leyrat <mleyrat@npr.org>
2024-05-07 19:12:05 -0700

    Initial Commit - koa boilerplate

---
+++ koa-oauth/.eslintrc.js
@@ -0,0 +1,10 @@
+export default {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // add any custom rules here
+  },
+};

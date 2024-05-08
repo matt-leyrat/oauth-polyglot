f49d2482577fef81a6687ab6e98b507f8467ebff
Matt Leyrat <mleyrat@npr.org>
2024-05-08 11:51:24 -0700

    Clean up app.ts

--- koa-oauth/src/app.ts
+++ koa-oauth/src/app.ts
@@ -1,32 +1,28 @@
 import Koa from 'koa';
 import bodyParser from 'koa-bodyparser';
 
-import { sequelize } from './config/db';
+import { initializeDb } from './config/db';
 import authRouter from './routes/auth';
 import protectedRouter from './routes/protected';
 
 const app = new Koa();
 
-async function main() {
-  try {
-    // 🔌 connect to db
-    await sequelize.authenticate();
-    console.log('Database connection has been established successfully.');
-    await sequelize.sync(); // Sync models with the database
-    console.log('Models synced with the database.');
-    // ✅ register routes
-    app.use(bodyParser());
-    app.use(authRouter.routes());
-    app.use(authRouter.allowedMethods());
-    app.use(protectedRouter.routes());
-    app.use(protectedRouter.allowedMethods());
-    // 🚀 start server
-    app.listen(3000, () => {
-      console.log('Server is running on port 3000');
-    });
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
+function registerRoutes() {
+  app.use(bodyParser());
+  app.use(authRouter.routes());
+  app.use(authRouter.allowedMethods());
+  app.use(protectedRouter.routes());
+  app.use(protectedRouter.allowedMethods());
 }
 
-main();
+async function main() {
+  await initializeDb();
+  registerRoutes();
+  app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
+}
+
+main()
+  .then(() => console.log('koa-oauth: ready to rumble!'))
+  .catch((e) => console.error(e));
